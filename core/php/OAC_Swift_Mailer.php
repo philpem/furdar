e2b748e8f4f5038ca2b41c102b209cf47df17146
Saul Cozens <saul@sheffield.digital>
2019-12-18 09:02:38 +0000

    fix to allow SwiftMailer to recover from an SMTP connection timeout

--- core/php/OAC_Swift_Mailer.php
+++ core/php/OAC_Swift_Mailer.php
@@ -12,15 +12,8 @@ class OAC_Swift_Mailer extends Swift_Mailer
 {
     public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
-
-        // https://github.com/symfony/swiftmailer-bundle/issues/39
-        // https://github.com/OpenACalendar/OpenACalendar-Web-Core/issues/767
-        try {
-            parent::send($message, $failedRecipients);
-        } catch (Exception $e) {
-            parent::getTransport()->stop();
-            parent::getTransport()->start();
-            parent::send($message, $failedRecipients);
-        }
+        $result=parent::send($message, $failedRecipients);
+        parent::getTransport()->stop();
+        return $result;
     }
 }
