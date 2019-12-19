d1ed7440cf48abdad82ab849ebdf5ee5218d5a57
Saul Cozens <saul@sheffield.digital>
2019-12-19 17:22:04 +0000

    added some explaination for the Swift_Mailer timeout fix.

--- core/php/OAC_Swift_Mailer.php
+++ core/php/OAC_Swift_Mailer.php
@@ -12,6 +12,14 @@ class OAC_Swift_Mailer extends Swift_Mailer
 {
     public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
+
+        /* Swift_Mailer has known issues with dropped SMTP connections. This is not usually a problem 
+        ** except for long lived cases when sending through very fussy SMTP gateways such as AWS SES.
+        ** To mitigate this is is advisable to stop the connection after each send. Swift_Mailer will 
+        ** automatically re-establish the connection on the next send. This has a performance 
+        ** hit if you're sending a LOT of emails, but we won't notice much difference for this app.
+        */
+
         $result=parent::send($message, $failedRecipients);
         parent::getTransport()->stop();
         return $result;
